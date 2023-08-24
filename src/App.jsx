732759e4feb88d3be7d435cd16c66966a2045104
Victoria Ebong <79677353+victoriaEssien@users.noreply.github.com>
2023-08-24 22:14:16 +0100

    created and designed input field and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 
+// React Hooks
+
+
+// Bootstrap Components
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 
 function App() {
+
+  
+  
   return (
-    <div>
-      <div></div>
+    <div className="main-container">
+        <h3 className="logo">devfinder</h3>
+        <div className='input-container'>
+          <Form>
+            <Form.Control size="lg" className="input-field" type="text" placeholder="Search Github username..." />
+            <Button type='button' className='submit-btn'>Search</Button>
+          </Form>
+        </div>
     </div>
   )
 }
