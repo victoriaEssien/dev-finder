184868f7bcec043c181581b4204d92c128ffa8a2
Victoria Ebong <79677353+victoriaEssien@users.noreply.github.com>
2023-08-24 23:53:19 +0100

    finished styling user details profile card

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import { useState } from 'react';
 // Bootstrap Components
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import Table from 'react-bootstrap/Table';
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 
 // FontAwesome Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -39,53 +40,59 @@ function App() {
         </div>
 
         <div className="profile-container">
-          <div className="avatar-container">
-            <img src="https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj" className='avatar' alt="" />
-          </div>
-
-          <div className="user-details">
-            <div className="top-container">
-              <h2 className='name'>The Octocat</h2>
-              <span>Joined 25 Jan 2011</span>
-            </div>
-            <p className='username'>@octocat</p>
-            <p className='bio'>This profile has no bio</p>
-            <div className="git-stats">
-              <div className="stats-header">
-                <span>Repos</span>
-                <span>Followers</span>
-                <span>Following</span>
+          <Row>
+            <Col lg={3}>
+              <div className="avatar-container">
+                <img src="https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj" className='avatar' alt="" />
               </div>
+            </Col>
 
-              <div className="stats-data">
-                <span>8</span>
-                <span>3938</span>
-                <span>9</span>
-              </div>
-            </div>
+            <Col>
+              <div className="user-details">
+                <div className="top-container">
+                  <h3 className='name'>The Octocat</h3>
+                  <span className='date-joined'>Joined 25 Jan 2011</span>
+                </div>
+                <p className='username'>@octocat</p>
+                <p className='bio'>This profile has a really long bio, so long that I'm not really sure if it would be able to fit in this tinnie tiny space.</p>
+                <div className="git-stats">
+                  <div className="stats-header">
+                    <span>Repos</span>
+                    <span>Followers</span>
+                    <span>Following</span>
+                  </div>
 
-            <div className="contact-container">
-              <div className="links-one">
-                <span>
-                  <FontAwesomeIcon icon={faMapMarkerAlt} /> San Francisco
-                </span>
-              
-                <span>
-                  <FontAwesomeIcon icon={faTwitter}/> https://mytwitter.com
-                </span>
-              </div>
+                  <div className="stats-data">
+                    <span>8</span>
+                    <span>3938</span>
+                    <span>9</span>
+                  </div>
+                </div>
 
-              <div className="links-one">
-                <span>
-                  <FontAwesomeIcon icon={faLink} /> https://mylink.com
-                </span>
-              
-                <span>
-                  <FontAwesomeIcon icon={faBuilding}/> agithub
-                </span>
+                <div className="contact-container">
+                  <div className="links-one">
+                    <span>
+                      <FontAwesomeIcon icon={faMapMarkerAlt} /> San Francisco
+                    </span>
+                  
+                    <span>
+                      <FontAwesomeIcon icon={faTwitter}/> https://mytwitter.com
+                    </span>
+                  </div>
+
+                  <div className="links-one">
+                    <span>
+                      <FontAwesomeIcon icon={faLink} /> https://mylink.com
+                    </span>
+                  
+                    <span>
+                      <FontAwesomeIcon icon={faBuilding}/> agithub
+                    </span>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            </Col>
+          </Row>
         </div>
     </div>
   )
