f13f67e22f62ec030e04e1f78c185df4b7a10bf9
Victoria Ebong <79677353+victoriaEssien@users.noreply.github.com>
2023-08-25 01:01:28 +0100

    added validation logic

--- src/App.jsx
+++ src/App.jsx
@@ -21,12 +21,13 @@ function App() {
 
   const [generateuserData, setGenerateUserData] = useState(null)
   const [username, setUsername] = useState("")
+  const [showProfile, setShowProfile] = useState(false);
 
   const fetchUserData = (e) => {
     e.preventDefault()
     Axios.get(`https://api.github.com/users/${username}`).then((res) => {
       setGenerateUserData(res.data);
-      console.log(res.data);
+      setShowProfile(true)
     })
   }
 
@@ -35,9 +36,21 @@ function App() {
   const formatYearMonth = (dateString) => {
     const date = new Date(dateString);
     const year = date.getFullYear();
-    const month = date.toLocaleString('default', { month: 'long' });
+    const month = date.toLocaleString('default', { month: 'short' });
     return `${month} ${year}`;
   };
+
+  const renderFieldValue = (value, label) => {
+    if (value === null || value === undefined) {
+      return (
+        <span className="null-value">
+          No {label.toLowerCase()} available
+        </span>
+      );
+    }
+    return value;
+  };
+
   
   return (
     <div className="main-container">
@@ -49,54 +62,62 @@ function App() {
           </Form>
         </div>
 
-        <div className="profile-container">
-          <Row>
-            <Col lg={3}>
-              <div className="avatar-container">
-                <img src={generateuserData?.avatar_url} className='avatar' alt="user's profile picture" />
-              </div>
-            </Col>
-
-            <Col>
-              <div className="user-details">
-                <div className="top-container">
-                  <h3 className='name'>{generateuserData?.name}</h3>
-                  <span className='date-joined'>Joined {generateuserData?.created_at && formatYearMonth(generateuserData.created_at)}</span>
+        {showProfile && generateuserData && (
+            <div className="profile-container">
+            <Row>
+              <Col lg={3}>
+                <div className="avatar-container">
+                  <img src={generateuserData?.avatar_url} className='avatar' alt="user's profile picture" />
                 </div>
-                <p className='username'>@{generateuserData?.login}</p>
-                <p className='bio'>{generateuserData?.bio}</p>
-                <div className="git-stats">
-                  <div className="stats-header">
-                    <span>Repos</span>
-                    <span>Followers</span>
-                    <span>Following</span>
+              </Col>
+
+              <Col>
+                <div className="user-details">
+                  <div className="top-container">
+                    <h3 className='name'>{generateuserData?.name}</h3>
+                    <span className='date-joined'>Joined {generateuserData?.created_at && formatYearMonth(generateuserData.created_at)}</span>
+                  </div>
+                  <p className='username'>@{generateuserData?.login}</p>
+                  <p className="bio">
+                  {renderFieldValue(generateuserData?.bio, 'Bio')}
+                </p>
+                  <div className="git-stats">
+                    <div className="stats-header">
+                      <span>Repos</span>
+                      <span>Followers</span>
+                      <span>Following</span>
+                    </div>
+
+                    <div className="stats-data">
+                      <span>{generateuserData?.public_repos}</span>
+                      <span>{generateuserData?.followers}</span>
+                      <span>{generateuserData?.following}</span>
+                    </div>
                   </div>
 
-                  <div className="stats-data">
-                    <span>{generateuserData?.public_repos}</span>
-                    <span>{generateuserData?.followers}</span>
-                    <span>{generateuserData?.following}</span>
+                  <div className="contact-container">
+                    <div className="links-one">
+                    {generateuserData?.location && (
+                      <span>
+                        <FontAwesomeIcon icon={faMapMarkerAlt} />{' '}
+                        {generateuserData?.location}
+                      </span>
+                    )}
+                    
+                    {generateuserData?.twitter_username && (
+                      <span>
+                        <FontAwesomeIcon icon={faTwitter} />{' '}
+                        {generateuserData?.twitter_username}
+                      </span>
+                    )}
+                    </div>
                   </div>
+                  <a href={generateuserData?.html_url} target='_blank' rel='noreferrer' className='view-profile-btn'>View full profile</a>
                 </div>
-
-                <div className="contact-container">
-                  <div className="links-one">
-                    <span>
-                      <FontAwesomeIcon icon={faMapMarkerAlt} /> {generateuserData?.location}
-                    </span>
-                  
-                    <span>
-                      <FontAwesomeIcon icon={faTwitter}/> {generateuserData?.twitter_username}
-                    </span>
-                  </div>
-                </div>
-                <div className="btn">
-                <a href={generateuserData?.html_url} target='_blank' rel='noreferrer' className='view-profile-btn'>View full profile</a>
-                </div>
-              </div>
-            </Col>
-          </Row>
-        </div>
+              </Col>
+            </Row>
+          </div>
+        )}
     </div>
   )
 }
